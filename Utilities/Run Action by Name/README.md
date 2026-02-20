ae45e8deea949422dcc7751c390e14252871c3ac
Phai Ting <phaiting@phaiting.com>
2026-02-20 17:04:54 -0500

    Updated the code to allow the Action to check for the existence of an Action without actually running it.

--- Utilities/Run Action by Name/README.md
+++ Utilities/Run Action by Name/README.md
@@ -4,3 +4,6 @@ This StreamerBot Action allows you to run Actions based on the value in a variab
 Set the name of the Action in the "runActionName" variable and then run this Action if it exists.
 If there was an Action with the name specified then the "runActionResult" will equal true, otherwise it will 
 be false.
+
+To use this Action to check for the existence without actually running the Action, set the variable 
+"runActionNameCheckOnly" to "True".
--- Utilities/Run Action by Name/Run Action by Name.cs
+++ Utilities/Run Action by Name/Run Action by Name.cs
@@ -5,11 +5,20 @@ public class CPHInline
 	public bool Execute()
 	{
 		CPH.TryGetArg("runActionName", out string runActionName);
+		CPH.TryGetArg("runActionNameCheckOnly", out string checkOnly);
 		if (CPH.ActionExists(runActionName))
     	{
 			// Code to execute if the action exists
 			CPH.LogDebug($"The action '{runActionName}' exists.");
-			CPH.RunAction(runActionName); // You can then run the action
+			if (!"true".Equals(checkOnly, StringComparison.OrdinalIgnoreCase))
+			{
+				// The Action exists and this is not a Check Only. Runt it.
+				CPH.RunAction(runActionName);
+			}
+			else
+			{
+				CPH.LogDebug($"Check Only was set to '{checkOnly}'.");
+			}
 			CPH.SetArgument("runActionResult", true);
 		}
 		else
